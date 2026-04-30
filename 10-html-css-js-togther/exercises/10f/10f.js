4f878e47e6155c3c5b369494d0701c4b603f2291
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-04-30 09:46:11 +0530

    js-lessons-copy

---
+++ 10-html-css-js-togther/exercises/10f/10f.js
@@ -0,0 +1,8 @@
+ function buttons(selector){
+      const button = document.querySelector(selector);
+    if(!button.classList.contains('is-toggled')){
+      button.classList.add('is-toggled');
+    } else {
+      button.classList.remove('is-toggled');
+    }
+     }
