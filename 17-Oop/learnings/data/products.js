e76a52fd23c7cfdc12db843473722a6547f7a52c
piyushdhakad001 <piyushdhakad627@gmail.com>
2026-05-25 23:02:49 +0530

    Convert product into classes

--- 17-Oop/learnings/data/products.js
+++ 17-Oop/learnings/data/products.js
@@ -1,3 +1,5 @@
+ 
+ import { formatCurrency } from "../scripts/utils/money.js";
  export function getProduct(productId){
   let matchingProduct;
   
@@ -9,6 +11,46 @@
         return matchingProduct;
  }
  
+
+ class product {
+  id;
+  image;
+  name;
+  rating;
+  priceents;
+
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+  getStarsUrl(){
+  return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+  getPrice(){
+   return `$${formatCurrency(this.priceCents)}`;
+
+  }
+ }
+ const product1 = new product( {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    rating: {
+      stars: 4.5,
+      count: 87
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },);
+ 
+
  export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -719,4 +761,6 @@
     ]
   }
 
-];
+].map((productDetails) => {
+   return new product(productDetails);
+});
